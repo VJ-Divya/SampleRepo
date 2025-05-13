e0cc85d5300aeb9bb122b7d0bb4fad8165ece6d1
VJ-Divya <diyaraghav2806@gmail.com>
2025-05-13 11:02:25 +0530

    message

---
+++ src/org/samplecode/SamCode.java
@@ -0,0 +1,13 @@
+package org.samplecode;
+
+public class SamCode {
+	
+	private void m1() {
+		System.out.println("first method");
+	}public static void main(String[] args) {
+		SamCode sc=new SamCode();
+		sc.m1();
+	}
+	
+
+}
